f6ca1f84a525da006a641eadd366ae82a71a3daa
dvmoomoodv <dvmoomoodv@gmail.com>
2017-05-25 19:14:27 +0900

    edit user

--- vendor/codeigniter/framework/application/controllers/User.php
+++ vendor/codeigniter/framework/application/controllers/User.php
@@ -2,6 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
+	function __construct() {
+		parent::__construct ();
+		set_language();
+		$this->version = "alpha/";
+		$this->category = "intro/";
+		$this->layout_path = $this->version."layout/";
+		$this->view_path = $this->version.$this->category;
+
+		$this->layout->setLayout($this->layout_path."intro_layout_view");
+	}
 	public function index()
 	{
 		// $this->load->model('Welcome_model');
