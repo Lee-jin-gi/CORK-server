dcddc922e372c7d40d1542af247b7a97902e454e
dvmoomoodv <dvmoomoodv@gmail.com>
2017-05-31 11:24:18 +0900

    test code

--- vendor/codeigniter/framework/application/controllers/User.php
+++ vendor/codeigniter/framework/application/controllers/User.php
@@ -4,22 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class User extends CI_Controller {
 	function __construct() {
 		parent::__construct ();
-		// set_language();
-		$this->version = "alpha/";
-		$this->category = "intro/";
-		$this->layout_path = $this->version."layout/";
-		$this->view_path = $this->version.$this->category;
-
-		$this->layout->setLayout($this->layout_path."intro_layout_view");
+		$this->layout->setLayout("layout/user_layout_view");
 	}
 	public function index()
 	{
-		// $this->load->model('Welcome_model');
-		// $test_info = $this->Welcome_model->select_test();
-		// $data["test_info"] = $test_info;
-		// $this->load->view('welcome_message',$data);
-		// echo "user";
-		$this->layout->setLayout("layout/user_layout_view");
 		$this->layout->view("/user/main_view");
 	}
 	function test(){
